20dbe337db24ca9fb7fffc5fde9113c2f37b6d04
Michael Hunger <github@jexp.de>
2024-11-27 10:47:51 +0100

    Change site to graphrag.com for canonical URL etc

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://graphrag.github.io',
+  site: 'https://graphrag.com',
   integrations: [starlight({
     title: 'GraphRAG',
     logo: {
